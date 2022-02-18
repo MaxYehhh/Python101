09e125138bb4507fe39ef61ce799b1001874c6a8
Max <max93610@gmail.com>
2022-02-18 12:19:04 +0800

    upload

--- Programming for Business Computing in Python (1)/W2_how to make change.py
+++
@@ -1,9 +0,0 @@
-expense = int(input())
-change = 1000 - expense
-a = change // 500
-b = (change - 500*a) // 100
-c = (change - 500*a - 100*b) // 50
-d = (change - 500*a - 100*b - 50*c) // 10
-e = (change - 500*a - 100*b - 50*c - 10*d) // 5
-f = (change - 500*a - 100*b - 50*c - 10*d - 5*e) // 1
-print(a,b,c,d,e,f)
